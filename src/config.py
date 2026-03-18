78ab6db10df5048f43122cbe806e52b3945cb8a1
Anton Lehnerer <antonleh04@gmail.com>
2026-03-18 11:47:48 +0100

    update config

--- src/config.py
+++ src/config.py
@@ -1,6 +1,9 @@
 import os
 
 from aeon.classification.convolution_based import RocketClassifier
+from aeon.classification.convolution_based import MiniRocketClassifier
+from aeon.transformations.collection.convolution_based import MultiRocketClassifier
+from aeon.classification.convolution_based import MultiRocketHydraClassifier
 from aeon.classification.distance_based import KNeighborsTimeSeriesClassifier
 from aeon.classification.shapelet_based import ShapeletTransformClassifier
 from aeon.classification.dictionary_based import BOSSEnsemble
@@ -11,21 +14,27 @@ from aeon.classification.feature_based import Catch22Classifier
 # --- Datasets ---
 DATASETS = [
     "Computers",
-    "ECG200",
-    "DistalPhalanxTW",
+    "HandOutlines"
+    "MiddlePhalanxOutlineCorrect"
+    "WormsTwoClasses"
     "Worms",
-    "Earthquakes"
+    "Earthquakes",
+    "EMOPain",
+    "EthanolLevel"
 ]
 
 # --- Classifiers ---
 CLASSIFIERS = {
     "ROCKET":       lambda: RocketClassifier(random_state=RANDOM_SEED),
+    "MiniROCKET":   lambda: MiniRocketClassifier(random_state=RANDOM_SEED),
+    "MultiROCKET":   lambda: MultiRocketClassifier(random_state=RANDOM_SEED),
+    "MultiROCKETHydra": lambda: MultiRocketHydraClassifier(random_state=RANDOM_SEED),
     "1NN-ED":       lambda: KNeighborsTimeSeriesClassifier(n_neighbors=1, distance="euclidean"),
     "1NN-DTW":      lambda: KNeighborsTimeSeriesClassifier(n_neighbors=1, distance="dtw"),
     "ST":           lambda: ShapeletTransformClassifier(random_state=RANDOM_SEED),
     "BOSS":         lambda: BOSSEnsemble(random_state=RANDOM_SEED),
     "TSF":          lambda: TimeSeriesForestClassifier(random_state=RANDOM_SEED),
-    "InceptionTime": lambda: InceptionTimeClassifier(random_state=RANDOM_SEED),
+    "InceptionTime": lambda: InceptionTimeClassifier(random_state=RANDOM_SEED, n_epochs=100),    #default n_epochs is 1500
     "catch22":      lambda: Catch22Classifier(random_state=RANDOM_SEED),
 }
 
